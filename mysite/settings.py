661eb997c766f80b8111b33066069c2cde970d4a
Sean Abbas <sean.abbas33@gmail.com>
2024-09-18 13:25:04 -0400

    add Task model

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'taskade.apps.TaskadeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- taskade/models.py
+++ taskade/models.py
@@ -1,3 +1,17 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Task(models.Model):
+    name = models.CharField(max_length=200)
+    created_time = models.DateTimeField()
+    due_date = models.DateField()
+    priority = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+    
+    def was_published_recently(self):
+        return self.created_time >= timezone.now() - datetime.timedelta(days=1)
