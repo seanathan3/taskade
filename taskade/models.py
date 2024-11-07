308e1d269ebbec9681bf32dc5d65a499ec60952a
seanathan3 <sean.abbas33@gmail.com>
2024-11-07 14:35:28 -0500

    through 8 in net ninja

--- taskade/models.py
+++ taskade/models.py
@@ -1,17 +1,17 @@
 import datetime
 from django.db import models
-from django.utils import timezone
 
 # Create your models here.
 
 class Task(models.Model):
     name = models.CharField(max_length=200)
-    created_time = models.DateTimeField()
+    created_date = models.DateField(auto_now_add=True)
     due_date = models.DateField()
     priority = models.IntegerField()
+    # add in owner later
 
     def __str__(self):
         return self.name
     
-    def was_published_recently(self):
-        return self.created_time >= timezone.now() - datetime.timedelta(days=1)
+    def overdue(self):
+        return self.due_date < datetime.date.today()
