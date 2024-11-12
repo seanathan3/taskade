27720fb73f10aea4dcbd2aeff85370c75bca7d39
seanathan3 <sean.abbas33@gmail.com>
2024-11-12 11:53:14 -0500

    populate data in index

--- taskade/views.py
+++ taskade/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Task
 
 # Create your views here.
 
 def index(request):
-    # return HttpResponse("Welcome to TASKADE!")
-    return render(request, "taskade/index.html")
+    tasks = Task.objects.all().order_by("due_date")
+    return render(request, "taskade/index.html", {'tasks': tasks})
