eab0437415fb622d6ce8c2a125f6ba2d42b9fb23
seanathan3 <sean.abbas33@gmail.com>
2024-11-25 15:36:07 -0500

    check login status and redirect to home page

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,11 +20,14 @@ from django.urls import include, path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+from . import views
+from taskade import views as task_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('taskade/', include("taskade.urls")),
-    path("accounts/", include("accounts.urls"))
+    path("accounts/", include("accounts.urls")),
+    path("", task_views.index, name="home")
 ]
 
 urlpatterns += staticfiles_urlpatterns()
